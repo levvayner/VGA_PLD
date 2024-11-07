c6d36ee2b4cce3901212fea82a98cd71cd4b3846
Lev Vayner <lev.vayner@vaynersystems.com>
2024-11-07 18:08:27 -0500

    wip

--- BitmapMapper/Program.cs
+++ BitmapMapper/Program.cs
@@ -3,42 +3,98 @@ using Aspose.Drawing;
 using Aspose.Drawing.Imaging;
 internal static class Program
 {
+    public const string ARDUINO_PORT = "/dev/ttyACM0";
+
+    public static int BitmapWidth = 160;
+    public static int BitmapPadWidth = 51;
+    public static int BitmapHeight = 255;
+    public static int BitmapPadHeight = 6;
+    public static bool FillRAM = true;
     private static string InputFile;
     private static string OutputFile;
+    private static bool UseArduinoProxy = false;
+    
     private static void Main(string[] args)
     {
-        
-        if(args.Length < 2){
+        bool ReadCommand = false;
+
+        if (args.Length < 2)
+        {
             Console.WriteLine("Usage:");
             Console.WriteLine("BitmapMapper -i inputfile.bmp [-o outputfile.bin]");
+            //CreateImageBin(800,600);
+            InputFile = "puppy.png";
+            UseArduinoProxy = true;
             WriteStripes();
-            return;        
+            return;
+            //ReadRAM();
         }
-        for(int i = 0;args.Length > i;i++){
-            if(args[i] == "-i"){
+        for (int i = 0; args.Length > i; i++)
+        {
+            if (args[i] == "-i")
+            {
                 InputFile = args[++i];
             }
-            if(args[i] == "-o"){
+            if (args[i] == "-o")
+            {
                 OutputFile = args[++i];
             }
+            if (args[i] == "-p" || args[i] == "--program")
+            {
+                UseArduinoProxy = true;
+            }
+
+            if (args[i] == "-r" || args[i] == "--read")
+            {
+                ReadCommand = true;
+            }
         }
-        if(string.IsNullOrEmpty(OutputFile)){
+        if (string.IsNullOrEmpty(InputFile))
+        {
+            InputFile = "colors.bin";
+        }
+        if (string.IsNullOrEmpty(OutputFile))
+        {
             OutputFile = Path.GetFileNameWithoutExtension(InputFile) + ".bin";
         }
-        Console.WriteLine($"Creating {OutputFile} from {InputFile}");
 
-        try{
+        if (ReadCommand)
+        {
+            ReadRAM();
+            return;
+        }
+        Console.WriteLine($"Creating {OutputFile} from {InputFile}");
+        var created = CreateImageBin(BitmapWidth, BitmapHeight);
+
+        if (UseArduinoProxy && created)
+        {
+            //progam using arudino device
+
+            var reader = new PortReader(OutputFile);
+            reader.WriteBinary(FillRAM);
+        }
+
+    }
+
+    private static bool CreateImageBin(int width = 160, int height = 120)
+    {
+        try
+        {
             Bitmap bmp = new Bitmap(InputFile);
-            Bitmap resized = new Bitmap(bmp, 86, 64);
-            var length = resized.Height * resized.Width;
+            Bitmap resized = new Bitmap(bmp, width, height);
+            var length = resized.Height  * 256 ;
             byte[] bytes = new byte[length]; // 1 byte per pixel
-            for(int y = 0; y < resized.Height ;y++) {
-                for(int x = 0; x < resized.Width;x++) {
-                    var color = resized.GetPixel(x,y);
-                    bytes[y * resized.Width + x] = (byte)(color.R/32 | color.G/32 << 3 | color.B/64 << 6  );
+            for (int y = 0; y < resized.Height; y++)
+            {
+                Console.WriteLine("Resizing row " + y);
+                for (int x = 0; x < resized.Width; x++)
+                {
+                    var color = resized.GetPixel(x, y);
+                    bytes[(y << 7) + x] = (byte)(((color.R/ 32) & 0x3) | ((color.G / 32) & 0x3) << 3 | (color.B / 64) << 6);
                 }
             }
-            
+            Console.WriteLine($"Resized bitmap to {width}x{height}");
+
             // BitmapData data = resized.LockBits(new Rectangle(0,0,resized.Width, resized.Height),ImageLockMode.ReadOnly,PixelFormat.Format8bppIndexed);
             // var length = data.Stride * data.Height;
 
@@ -51,94 +107,82 @@ internal static class Program
             // bmp.Dispose();
 
             Console.WriteLine($"Read {length} bytes from {InputFile}. Creating binary...");
-            using(BinaryWriter writer = new BinaryWriter(new FileStream(OutputFile, FileMode.OpenOrCreate))){
+            using (BinaryWriter writer = new BinaryWriter(new FileStream(OutputFile, FileMode.Create)))
+            {
                 writer.Write(bytes);
                 writer.Close();
             }
 
             Console.WriteLine($"Completed writing data to {OutputFile}");
+            resized.Save(Path.GetFileNameWithoutExtension(OutputFile) + "Resized.bmp");
+            return true;
         }
-        catch(Exception ex){
-            Console.WriteLine("Error occured generating data: " + ex.Message);
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error occured generating data: " + ex.Message + "\n" + ex.StackTrace);
+            return false;
         }
-
     }
 
     private static void WriteStripes()
     {
-        // counter will count until 160 for visible, 211 total horizontally
-        // counter will count until 600 for visible, 628 total vertically
-        // shifting horizontal one bit, we get 80 and 106 respectivly
-        // shifting vertical three bits, we get 75 and 78 respectivly
-        // draw horizontal for 80, blank until 106
-        // draw vertical for 75, blank until 78
-
-        // var fullWidth = 106;
-        // var drawWidth = 80;
-        // var fullHeight = 78;
-        // var drawHeight = 75;
-        
-        // var length = fullHeight * fullWidth;
-        // byte[] bytes = new byte[length]; // 1 byte per pixel
-        // for(int y = 0; y < fullHeight ;y++) {
-        //     Console.Write($"{string.Format( "0x{0:X4}", y*fullWidth)}");
-        //     for(int x = 0; x < fullWidth;x++) {
-        //         var red = 1;
-        //         var green = 0;// x > drawWidth/2 && y < drawHeight / 2 ? 1 : 0;
-        //         var blue = 0;//x > drawWidth/2 && y > drawHeight / 2 ? 1 : 0;
-                
-        //         if(x > 5 && x < drawWidth - 10 && y < (drawHeight /2 + 1) && y > (drawHeight /2 - 5))
-        //             bytes[y*fullWidth + x] = (byte)(red | green << 3 | blue << 6);
-        //         else
-        //             bytes[y*fullWidth + x] = 0x0;
-                
-        //         Console.Write($"{ string.Format( "{0:X2}", bytes[y*fullWidth + x])} ");                    
-        //     }
-        //     Console.WriteLine();
-        // }
-
-        
-        var drawWidth = 80;
-        var drawHeight = 120;
         
         //var length = drawHeight * drawWidth;
-        var length = 32*1024;
+        var length = 64*1024;
         byte[] bytes = new byte[length]; // 1 byte per pixel
         for(int idx = 0; idx < length;) {
             bytes[idx++] = (byte)0;
         }
         
-        for(int y = 0; y < drawHeight ;y++) {
-            Console.Write($"{string.Format( "0x{0:X4}", y*drawWidth)}");
-            for(int x = 0; x < drawWidth;x++) {
-                var blockX = x / 5;
-                var blockY = y/5;
-                var red = blockX;
-                var green = blockX / 2;// x > drawWidth/2 && y < drawHeight / 2 ? 1 : 0;
-                var blue = blockY;//x > drawWidth/2 && y > drawHeight / 2 ? 1 : 0;
+        for(int y = 0; y < BitmapHeight ;y++) {
+            //Console.Write($"{string.Format( "0x{0:X4}", y*drawWidth)}");
+            for(int x = 0; x < BitmapWidth;x++) {
+                // var blockX = x / 5;
+                // var blockY = y/5;
+                var red = y < (BitmapHeight / 2) ? 1 : 0;
+                var green = 0;// x > drawWidth/2 && y < drawHeight / 2 ? 1 : 0;
+                var blue = x > BitmapWidth/2 && y > BitmapHeight / 2 ? 1 : 0;
                 
                 //if(x < drawWidth && y < drawHeight /2 && x == drawWidth / 2 && x > 5 && y > 5)
                 //draw border 5 pixels from edges
-                var topBorder = x >= 5 && x <= drawWidth - 10 && y == 5;
-                var bottomBorder = x > 5 && x <= drawWidth - 10 && y == drawHeight - 10;
-                var leftBorder = x == 5 && y >= 5 && y <= drawHeight - 10;
-                var rightBorder = x == drawWidth - 10 && y >= 5 && y <= drawHeight - 10;
+                var topBorder = x >= 5 && x <= BitmapWidth - 10 && y == 5;
+                var bottomBorder = x > 5 && x <= BitmapWidth - 10 && y == BitmapHeight - 10;
+                var leftBorder = x == 5 && y >= 5 && y <= BitmapHeight - 10;
+                var rightBorder = x == BitmapWidth - 10 && y >= 5 && y <= BitmapHeight - 10;
 
                 if(leftBorder || rightBorder)
-                    bytes[y*drawWidth + x] = 1;
+                    bytes[y*256 + x] = 1;
                 else if (topBorder || bottomBorder)
-                    bytes[y*drawWidth + x] = 1 << 3;
-                else if(x > 5 && x < drawWidth - 10 && y > 5 && y < drawHeight - 10)
-                    bytes[y*drawWidth + x] =(byte)(red | green << 3 | blue << 6);
+                    bytes[y*256 + x] = 1 << 3;
+                else if(x < BitmapWidth && y < BitmapHeight)
+                    bytes[y*256 + x] =(byte)(red | green << 3 | blue << 6);
                 
-                Console.Write($" { string.Format( "{0:X2}", bytes[y*drawWidth + x])}");                    
+                //Console.Write($" { string.Format( "{0:X2}", bytes[y*drawWidth + x])}");                    
             }
-            Console.WriteLine();
+            //Console.WriteLine();
         }
-
-        using(BinaryWriter writer = new BinaryWriter(new FileStream(string.IsNullOrEmpty(OutputFile) ? "colors.bin" : OutputFile, FileMode.Create))){
+        OutputFile = string.IsNullOrEmpty(OutputFile) ? "colors.bin" : OutputFile;
+        using(BinaryWriter writer = new BinaryWriter(new FileStream(OutputFile, FileMode.Create))){
             writer.Write(bytes);
             writer.Close();
         }
+
+        if(UseArduinoProxy){
+            //progam using arudino device
+
+            var reader = new PortReader(OutputFile);
+            reader.WriteBinary(FillRAM);
+
+            Console.WriteLine("Reading...");
+
+            reader.ReadRam();
+            
+        }
+    }
+
+    private static void ReadRAM(){
+         var reader = new PortReader(OutputFile);
+            Console.WriteLine("Reading...");
+            reader.ReadRam();
     }
 }
